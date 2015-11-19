487b389175b645128a13b6dcc9d251bb20dff6fb
Florian <florian.leman@gmail.com>
2015-11-19 16:47:29 +0100

    x

--- Supplier/src/shared/Article.java
+++ Supplier/src/shared/Article.java
@@ -1,9 +1,17 @@
 package shared;
 
 public class Article {
-	public int id;
+	public String id;
 	public String name;
 	public String description;
 	public int quantity;
-	public float price;
+	public double price;
+	
+	public Article(String id, String name, String description, int quantity, double price){
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.quantity = quantity;
+		this.price = price;
+	}
 }
